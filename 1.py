3a43a163aaa786f1d655fa13e1d09c46e4b306c0
LuckYYadav15 <82745102+LuckYYadav15@users.noreply.github.com>
2021-11-18 23:47:23 +0530

    Add files via upload
    practise

---
+++ 1.py
@@ -0,0 +1,41 @@
+# from selenium import webdriver
+# PATH = 'C:\Program Files (x86)\chromedriver.exe'
+# driver = webdriver.Chrome(PATH)
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# import time 
+# driver.get("https://twitter.com/elonmusk")
+# time.sleep(3)
+# searchbox = driver.find_element_by_css_selector("input[placeholder='Search Twitter']")
+# searchbox.send_keys("Keshavjha018")
+# searchbox.send_keys(Keys.RETURN)
+# import os
+
+# print('Push "enter" to play movie')
+# #raw_input()
+import os
+def filename():
+   filename = C:\Users\Utkarsh Yadav\Desktop\selenium\movie.mkv
+   os.system("start " + filename)
+
+# open(filename)
+
+# def play_movie(path):
+#     from os import startfile
+#     startfile(movie.mp4)
+
+# class Video(object):
+#     def __init__(self,path):
+#         self.path = movie.mp4
+
+#     def play(self):
+#         from os import startfile
+#         startfile(self.path)
+
+# class Movie_MP4(Video):
+#     type = "MP4"
+
+# movie = Movie_MP4(r"C:\My Documents\My Videos\Heres_a_file.mp4")
+# if input("Press enter to play, anything else to exit") == '':
+#     movie.play()
+
